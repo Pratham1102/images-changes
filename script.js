a988d9f1ad82be934c569fcc740002a566330b63
Pratham <151525345+Pratham1102@users.noreply.github.com>
2024-05-02 22:55:52 +0530

    clone
    clone

---
+++ script.js
@@ -0,0 +1,20 @@
+function showimg1() {
+    document.getElementById("change-img").src = "images/img1.jpg";
+    document.getElementById("change-text").innerHTML =""
+}
+function showimg2() {
+    document.getElementById("change-img").src = "images/img2.jpg";
+    document.getElementById("change-text").innerHTML =""
+}
+function showimg3() {
+    document.getElementById("change-img").src = "images/img3.jpg";
+    document.getElementById("change-text").innerHTML =""
+}
+function showimg4() {
+    document.getElementById("change-img").src = "images/img4.jpg";
+    document.getElementById("change-text").innerHTML =""
+}
+function showimg5() {
+    document.getElementById("change-img").src = "images/img5.jpg";
+    document.getElementById("change-text").innerHTML =""
+}
